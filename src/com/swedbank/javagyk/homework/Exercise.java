5228c3201b5c721e6d8c6483de99601aad7dedd2
Benas Skirmantas <benas.skirmantas@swedbank.lt>
2019-10-03 09:04:23 +0300

    first commit

---
+++ src/com/swedbank/javagyk/homework/Exercise.java
@@ -0,0 +1,15 @@
+package com.swedbank.javagyk.homework;
+
+public class Exercise {
+    //1. Create a main method
+    public static void main(String[] args) {
+        //2. Create a String variable for your first name and assign value for it
+        String firstName = "Benas";
+        //3. Create a String variable for your last name and assign value for it
+        String lastName = "Skirmantas";
+        //4. Create a String variable for your job role and assign value for it
+        String jobRole = "Software Engineer";
+        //5. Print out the sentence like: Hello, I'm Benas Skirmantas, I'm Software Engineer
+        System.out.println("Hello, " + "I'm " + firstName + " " + lastName + ", I'm " + jobRole);
+    }
+}
